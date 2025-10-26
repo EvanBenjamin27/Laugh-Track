b0d04ee0ab1f21c1e7d9cf98b3391dbfbf6bc6c5
EvanBenjamin27 <evanben@whitinsvillechristian.org>
2025-10-26 09:54:37 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,53 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('laughtrack-v1').then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/style.css',
-        '/script.js',
-        '/images/icon-192x192.png'
-      ]);
+const CACHE_NAME = 'laughtrack-v2';
+const URLS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/tailwind.js',
+  '/bgv2.png',
+  '/Sintony-Bold.ttf',
+  '/Sintony-Regular.ttf',
+  '/images/icon-192x192.png'
+];
+
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(URLS_TO_CACHE);
     })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }))
+    )
+  );
+  self.clients.claim();
+});
+
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        
+        if (response.type === 'opaqueredirect') {
+          return fetch(event.request, { redirect: 'follow' });
+        }
+        
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
